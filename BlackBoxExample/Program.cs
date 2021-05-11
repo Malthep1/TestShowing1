1a0a732879c4df16c3dbee2b32738eecdf60e16d
Malthe Petersen <Malthep1@gmail.com>
2021-05-11 12:39:00 +0200

    BlackBoxExample

---
+++ BlackBoxExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BlackBoxExample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            AdvancedNumberAnalysisAlgortihm ANAA = new AdvancedNumberAnalysisAlgortihm();
+            Console.WriteLine(ANAA.simplePartWhichNeedsOutputFromComplexPart());
+        }
+    }
+
+    class AdvancedNumberAnalysisAlgortihm
+    {
+        public double insanelyComplexPartThatWillTakeAllYear(double input)
+        {
+            //Not yet implemented
+            return 3.134523;
+        }
+
+        public double simplePartWhichNeedsOutputFromComplexPart()
+        {
+            return 374 * insanelyComplexPartThatWillTakeAllYear(2.033);
+        }
+    }
+}
