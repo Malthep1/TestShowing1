63f0ebb43cc3f7c109000f25c321091a070ff6e8
Malthe Petersen <Malthep1@gmail.com>
2021-05-11 11:29:14 +0200

    TestClass and Console test of TestClass

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+//Author: 
+namespace TestConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            WhatAClass wat = new WhatAClass();
+            Console.WriteLine(wat.getTheString());
+        }
+    }
+/// <summary>
+/// Class holds a constant attribute, and a method for returning said attribute.
+/// </summary>
+    class WhatAClass
+    {
+        private String theString = "The one string";
+        
+        public String getTheString()
+        {
+            return theString;
+        }
+    }
+}
